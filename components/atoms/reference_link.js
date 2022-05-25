ea82351344b47e4f3fc95d811e1d30dee5d8c217
takumi_yamamoto <takumi_yamamoto@gdo.co.jp>
2022-05-26 03:19:23 +0900

    自己紹介と個人開発表示

--- components/atoms/reference_link.js
+++ components/atoms/reference_link.js
@@ -5,7 +5,9 @@ import Styles from "components/atoms/reference_link.module.scss";
 export default function ReferenceLink({url, label}) {
   return (
     <Link href={url}>
-      <a className={Styles.link} rel="noreferrer" target="_blank">{label}</a>
+      <a className={Styles.link} rel="noreferrer" target="_blank">
+        {label}
+      </a>
     </Link>
   );
 }
---
+++ components/molecules/introduction.js
@@ -0,0 +1,52 @@
+import Link from "next/link";
+
+import Styles from "components/molecules/introduction.module.scss";
+import LabelWithTitle from "components/atoms/label_with_title";
+import Business from "components/molecules/business";
+
+export default function Introduction() {
+  return (
+    <div className={Styles.container}>
+      <h2 className={Styles.title}>自己紹介</h2>
+      <span>
+        <LabelWithTitle title="氏名" content="山本 卓実" />
+        <LabelWithTitle title="生年月日" content="1992/06/20" />
+        <LabelWithTitle title="得意業務" content="Androidアプリ開発" />
+        <LabelWithTitle title="保有資格" content={`データベーススペシャリスト試験
+応用情報技術者試験
+日商簿記2級`} />
+        <LabelWithTitle title="今後の展望" content={`自分の興味のあるドメインのサービスの開発に携わっていたいです。
+仕事の質は人生の質に直結すると考えているので自分が興味のあるサービスの開発に携わりたいです。
+
+開発自体は好きなのでプライベートでもおこなっていますし、新しい技術への関心もあるので日々色々な技術に触れるようにしていますが、特定の言語やFWに固執しているわけではなくサービスを開発/リリースすることそのものに重きを置いて業務を行いたいと考えます。`} />
+      </span>
+      <h3 className={Styles.title}>個人開発記録</h3>
+      <div className={Styles.salt}>
+        <Link href="https://salt-cooking.fun/">
+          <a rel="noreferrer" target="_blank">
+            <img className={Styles.salt_image} src="salt_top.png" />
+          </a>
+        </Link>
+        <Business
+          title="Salt"
+          start="2021年12月"
+          end="現在"
+          architecture={{
+            frameWork: "Next.js, Ruby on Rails",
+            language: "html, css, javascript, ruby, SQL",
+            db: "postgres",
+            versionControll: "Github"
+          }}
+          description={`料理のレシピ記録webサービスです。
+レシピの投稿や画像登録、devise token authを用いた認証機能、youtubeの動画情報のインポート機能やレシピ検索/いいね/お気に入り機能等を実装しました。
+プラットフォームはNext.jsで実装したフロントエンド部分はvercelにデプロイし、railsで実装したバックエンドはawsのec2へデプロイしています。
+docker composeでフロントエンド用、バックエンド用、DB用のコンテナを立てて開発しています`}
+          referenceProperty={{
+              url: "https://salt-cooking.fun/",
+              label: "webサイト"
+            }}
+          />
+      </div>
+    </div>
+  );
+}
